6aaa6648cc9dcf66f691b458e303c5dde571bdba
juanjoNtt <141405551+juanjoNtt@users.noreply.github.com>
2023-09-26 14:29:52 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,20 +6,32 @@ export default function App() {
     <div className="container">
       <h1>Título del formulario</h1>
       <form>
-        <label for="name">Nombre</label>
-        <input type="text" id="name" />
-        <label for="age">Edad</label>
-        <input type="number" id="age" />
-        <label for="password">Contraseña</label>
-        <input type="password" id="password" />
-        <label for="email">Email</label>
-        <input type="email" id="email" />
-        <label for="genre">Sexo</label>
-        <select id="genre">
-          <option value="macho">Macho</option>
-          <option value="hembra">Hembra</option>
-        </select>
-        <input type="checkbox" id="allAccepted" />
+        <fieldset>
+          <legend>Datos personales</legend>
+          <label for="name">Nombre</label>
+          <input type="text" id="name" aria-label="Nombre" />
+          <label for="age">Edad</label>
+          <input type="number" id="age" aria-label="Edad" />
+          <label for="genre">Sexo</label>
+          <select id="genre">
+            <option value="macho">Macho</option>
+            <option value="hembra">Hembra</option>
+          </select>
+        </fieldset>
+
+        <fieldset>
+          <legend>Cuenta</legend>
+          <label for="password">Contraseña</label>
+          <input type="password" id="password" aria-label="Contraseña" />
+          <label for="email">Email</label>
+          <input type="email" id="email" aria-label="Email" />
+        </fieldset>
+
+        <input
+          type="checkbox"
+          id="allAccepted"
+          aria-describedby="¿Has leido todo?"
+        />
         <label for="allAccepted">¿Has leido todo?</label>
         <button>Enviar</button>
       </form>
