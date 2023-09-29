ec8127b84c5912d9f9b612bcc5df91975eb87d7f
jgarcvaz <38000178+jgarcvaz@users.noreply.github.com>
2023-09-29 14:41:17 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
-import "./style.css";
-import { useForm } from "react-hook-form";
+import React from 'react';
+import './style.css';
+import { useForm } from 'react-hook-form';
 
 export default function App() {
-  const { register,  formState: { errors }, handleSubmit } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-  const onSubmit = (data) => { 
-    console.log(data) 
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
@@ -14,28 +18,45 @@ export default function App() {
       <h1>Título del formulario</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="name">Nombre</label>
-        <input type="text" id="name" {...register("name", { required: "Obligatorio", maxLength:{value: 20, message: "¿De verdad tienes un nombre tan largo?"}})} />
+        <input
+          type="text"
+          id="name"
+          {...register('name', {
+            required: 'Obligatorio',
+            maxLength: {
+              value: 20,
+              message: '¿De verdad tienes un nombre tan largo?',
+            },
+          })}
+        />
         {errors.name && <p role="alert">{errors.name?.message}</p>}
 
         <label htmlFor="age">Edad</label>
-        <input type="number" id="age" {...register("age", { 
-          required: "Obligatorio", min:{value:18, message: "Aún demasiado peque"}, max: {value:120, message: "Te has colado"} })} />
+        <input
+          type="number"
+          id="age"
+          {...register('age', {
+            required: 'Obligatorio',
+            min: { value: 18, message: 'Aún demasiado peque' },
+            max: { value: 120, message: 'Te has colado' },
+          })}
+        />
         {errors.age && <p role="alert">{errors.age?.message}</p>}
 
         <label htmlFor="password">Contraseña</label>
-        <input type="password" id="password" {...register("password", )} />
+        <input type="password" id="password" {...register('password')} />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register("email")} />
+        <input type="email" id="email" {...register('email')} />
 
         <label htmlFor="genre">Genero</label>
-        <select id="genre" {...register("genre")}>
+        <select id="genre" {...register('genre')}>
           <option value="male">Macho</option>
           <option value="female">Hembra</option>
           <option value="other">Otro</option>
         </select>
 
-        <input type="checkbox" id="allAccepted" {...register("accepted")} />
+        <input type="checkbox" id="allAccepted" {...register('accepted')} />
         <label htmlFor="allAccepted">¿Has leido todo?</label>
 
         <button type="submit">Enviar</button>
