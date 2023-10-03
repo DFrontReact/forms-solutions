c83d4d8117f503fd361c9022c6458bba9e8652b5
MatteoFranzini <129056176+MatteoFranzini@users.noreply.github.com>
2023-10-03 16:43:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import * as React from 'react';
 import './style.css';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { formValidation, validateValue } from './utils.js';
 
 export default function App() {
-  const [componentValue, setComponentValue] = React.useState('');
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
+  const [formValues, setFormValues] = React.useState('');
+  const initialValues = {
+    name: '',
+    age: 0,
+    password: '',
+    email: '',
+    genre: 'male',
+    allAccepted: false,
   };
 
-  const handleSubmit = (event) => {
-    alert('A name was submitted: ' + value);
-    event.preventDefault();
-  };
+  const validate = (values) => {};
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={value} onChange={handleChange} />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <div className="container">
+      <h1>Título del formulario</h1>
+      <Formik>
+        {({ isSubmitting }) => (
+          <Form>
+            <label htmlFor="name">Nombre</label>
+            <label htmlFor="age">Edad</label>
+            <label htmlFor="password">Contraseña</label>
+            <label htmlFor="email">Email</label>
+            <label htmlFor="genre">Sexo</label>
+            <label htmlFor="allAccepted">¿Has leido todo?</label>
+            <button type="submit" disabled={isSubmitting}>
+              Enviar
+            </button>
+          </Form>
+        )}
+      </Formik>
+      <p>Los valores recogidos del formulario son: {formValues}</p>
+    </div>
   );
 }
