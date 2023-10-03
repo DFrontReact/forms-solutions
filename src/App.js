fa4bcbcc20b228db6ecb3e134749ec09fb7385ad
jromerfr-ntt <128613443+jromerfr-ntt@users.noreply.github.com>
2023-10-03 21:05:16 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -7,14 +7,11 @@ export default function App() {
   const [formValues, setFormValues] = React.useState('');
   const initialValues = {
     name: '',
-    age: 0,
-    password: '',
-    email: '',
-    genre: 'male',
-    allAccepted: false,
   };
 
-  const formSchema = {};
+  const formSchema = Yup.object().shape({
+    name: Yup.string().required('Required').min(3),
+  });
 
   return (
     <div className="container">
@@ -32,24 +29,6 @@ export default function App() {
             <label htmlFor="name">Nombre</label>
             <Field type="text" name="name" />
             <ErrorMessage name="name" component="div" />
-            <label htmlFor="age">Edad</label>
-            <Field type="number" name="age" />
-            <ErrorMessage name="age" component="div" />
-            <label htmlFor="password">Contraseña</label>
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" />
-            <label htmlFor="email">Email</label>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-            <label htmlFor="genre">Sexo</label>
-            <Field as="select" name="genre">
-              <option value="male">Masculino</option>
-              <option value="female">Femenino</option>
-            </Field>
-            <ErrorMessage name="genre" component="div" />
-            <Field type="checkbox" name="allAccepted" />
-            <label htmlFor="allAccepted">¿Has leido todo?</label>
-            <ErrorMessage name="allAccepted" component="div" />
             <button type="submit" disabled={isSubmitting}>
               Enviar
             </button>
