ae20217f16adcfc10b41103057e457d8926db641
MatteoFranzini <129056176+MatteoFranzini@users.noreply.github.com>
2023-10-03 16:46:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './style.css';
 import { useForm } from 'react-hook-form';
 
 export default function App() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
+  const {} = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -21,42 +17,28 @@ export default function App() {
         <input
           type="text"
           id="name"
-          {...register('name', {
-            required: 'Obligatorio',
-            maxLength: {
-              value: 20,
-              message: '¿De verdad tienes un nombre tan largo?',
-            },
-          })}
         />
-        {errors.name && <p role="alert">{errors.name?.message}</p>}
 
         <label htmlFor="age">Edad</label>
         <input
           type="number"
           id="age"
-          {...register('age', {
-            required: 'Obligatorio',
-            min: { value: 18, message: 'Aún demasiado peque' },
-            max: { value: 120, message: 'Te has colado' },
-          })}
         />
-        {errors.age && <p role="alert">{errors.age?.message}</p>}
 
         <label htmlFor="password">Contraseña</label>
-        <input type="password" id="password" {...register('password')} />
+        <input type="password" id="password" />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register('email')} />
+        <input type="email" id="email" />
 
         <label htmlFor="genre">Genero</label>
-        <select id="genre" {...register('genre')}>
+        <select id="genre">
           <option value="male">Masculino</option>
           <option value="female">Femenino</option>
           <option value="other">Otro</option>
         </select>
 
-        <input type="checkbox" id="allAccepted" {...register('accepted')} />
+        <input type="checkbox" id="allAccepted" />
         <label htmlFor="allAccepted">¿Has leido todo?</label>
 
         <button type="submit">Enviar</button>
